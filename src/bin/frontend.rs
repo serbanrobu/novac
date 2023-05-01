01d6f5ede1e89ece4965b2bb9f544131d4ba1fbf
Șerban Robu <serbanrobu@protonmail.com>
2023-05-01 18:12:30 +0300

    update frontend

--- src/bin/frontend.rs
+++ src/bin/frontend.rs
@@ -1,4 +1,51 @@
-use leptos::{mount_to_body, view};
+use std::str::FromStr;
+
+use leptos::*;
+
+enum Kind {
+    Concatenation,
+    Literal,
+    Variable,
+}
+
+struct KindError;
+
+impl FromStr for Kind {
+    type Err = KindError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Concatenation" => Ok(Kind::Concatenation),
+            "Literal" => Ok(Kind::Literal),
+            "Variable" => Ok(Kind::Variable),
+            _ => Err(KindError),
+        }
+    }
+}
+
+#[component]
+fn Expression(cx: Scope) -> impl IntoView {
+    let (_kind, set_kind) = create_signal(cx, Kind::Literal);
+
+    view! {
+        cx,
+        <select
+            on:change=move |ev| {
+                let value = event_target_value(&ev);
+
+                let Ok(kind) = value.parse() else {
+                    return;
+                };
+
+                set_kind(kind);
+            }
+        >
+            <option>"Concatenation"</option>
+            <option selected>"Literal"</option>
+            <option>"Variable"</option>
+        </select>
+    }
+}
 
 fn main() {
     mount_to_body(|cx| {
